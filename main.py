902bca73fece8b7aa1f105a6557798d8ba1ee9b9
frivas3817 <56947502+frivas3817@users.noreply.github.com>
2019-12-07 23:22:17 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,46 +1,39 @@
-import os
-import csv
-file_to_load= os.path.join("Resources","election_data.csv")
-file_to_output= os.path.join("analysis","election_analysis.txt")
-total_votes = 0
-candidate_options = []
-candidate_votes = {}
-winning_candidate = ""
-winning_count = 0
+import os, csv
+file_to_load= os.path.join("Resources","budget_data.csv")
+file_to_output= os.path.join("analysis","budget_analysis.txt")
 
-with open(file_to_load) as election_data:
-    reader = csv.DictReader(election_data)
+tot_m= 0
+pre_rev=0
+total_rev=0
+month_of_change=[]
+rev_change_list=[]
+great_inc=["",0]
+great_dec=["",9999999999]
+
+with open(file_to_load) as revenue_data:
+    reader=csv.DictReader(revenue_data)
     for row in reader:
-        print(". ", end=""),
-        total_votes = total_votes + 1
-        candidate_name = row["Candidate"]
-        if candidate_name not in candidate_options:
-            candidate_options.append(candidate_name)
-            candidate_votes[candidate_name] = 0
-            candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
-            
-with open(file_to_output, "w") as txt_file:
-
-    election_results = (
-        f"\n\nElection Results\n"
-        f"-------------------------\n"
-        f"Total Votes: {total_votes}\n"
-        f"-------------------------\n")
-    print(election_results, end="")
-
-txt_file.write(election_results)
-    for candidate in candidate_votes:
-        votes = candidate_votes.get(candidate)
-        vote_percentage = float(votes) / float(total_votes) * 100
-        if (votes > winning_count):
-            winning_count = votes
-            winning_candidate = candidate
-voter_output = f"{candidate}: {vote_percentage:.3f}% ({votes})\n"
-print(voter_output, end="")
-        txt_file.write(voter_output)
-winning_candidate_summary = (
-        f"-------------------------\n"
-        f"Winner: {winning_candidate}\n"
-        f"-------------------------\n")
-    print(winning_candidate_summary)
-txt_file.write(winning_candidate_summary)
+        tot_m +=1
+        total_rev=int(row["Profit/Losses"])
+        rev_change=int(row["Profit/Losses"])- pre_rev
+        pre_rev=int(row["Profit/Losses"])
+        rev_change_list +=[rev_change]
+        month_of_change +=[row["Date"]]
+        if rev_change > great_inc[1]:
+            great_inc[0]=row["Date"]
+            great_inc[1]=rev_change
+        if rev_change < great_dec[1]:
+            great_dec[0]=row["Date"]
+            great_dec[1]=rev_change
+rev_avg=sum(rev_change_list)/ len(rev_change_list)
+output=(
+    f"\nFinancial Analysis\n"
+    f"===================\n"
+    f"Total Months: {tot_m}\n"
+    f"Total Revenue: ${total_rev}\n"
+    f'Averahe Revenue Change: ${rev_avg}\n"
+    f'Greatest Increase in Revenue: {great_inc[0]} ${great_inc[1]}\n"
+    f'Greatest Decrease in Revenue: {great_dec[0]} ${great_dec[0]}\n")
+print(output)
+with open(file_to_output,"w") as txt_file:
+    txt_file.write(output)
